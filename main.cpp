e32983ce54449f620f5ea3989c8b1d9f9ebbb6fe
ssanchkaa <sashamos111@gmail.com>
2024-11-20 19:45:29 +0500

    Вторая функция

--- main.cpp
+++ main.cpp
@@ -8,10 +8,11 @@ vector<string> readFile(const string& fileName)
 {
     return {};
 }
-
-void output(const vector<string>& lines)
+// Функция для вывода строк на экран
+void output(const std::vector<std::string>& lines)
 {
-
+    for (const auto& line : lines)
+        cout << line << std::endl;
 }
 
 void writeToFile(const vector<string>& lines, const string& fileName)
