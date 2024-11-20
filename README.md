a9ca1b3d0962d305f8827dc92328b02a03ad4d9a
Mikhail <laptev.misha2005@gmail.com>
2024-11-20 18:45:49 +0500

    Каркас

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # FirstLab
-# FirstLab
--- main.cpp
+++ main.cpp
@@ -1,9 +1,33 @@
 #include <iostream>
+#include <vector>
+#include <string>
 
 using namespace std;
 
+vector<string> readFile(const string& fileName)
+{
+    return {};
+}
+
+void output(const vector<string>& lines)
+{
+
+}
+
+void writeToFile(const vector<string>& lines, const string& fileName)
+{
+
+}
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    string inputFileName = "input.txt";
+    string outputFileName = "output.txt";
+
+    vector<string> lines = readFile(inputFileName);
+    Output(lines);
+    writeToFile(lines, outputFileName);
+
+    cout << "Программа выполнена";
     return 0;
 }
