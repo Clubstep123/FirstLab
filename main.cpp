5806966b1ea6c4820ae4a67a29ba192546c72472
ssanchkaa <sashamos111@gmail.com>
2024-11-20 20:09:16 +0500

    Третья функция

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
-
+#include <fstream>
 using namespace std;
 
 vector<string> readFile(const string& fileName)
@@ -9,15 +9,22 @@ vector<string> readFile(const string& fileName)
     return {};
 }
 // Функция для вывода строк на экран
-void output(const std::vector<std::string>& lines)
+void output(const vector<string>& lines)
 {
     for (const auto& line : lines)
-        cout << line << std::endl;
+        cout << line << endl;
 }
 
 void writeToFile(const vector<string>& lines, const string& fileName)
 {
-
+ofstream outputFile(fileName);
+    if (outputFile.is_open()) {
+        for (const auto& line : lines) {
+            outputFile << line << endl;
+        }
+        outputFile.close();
+    } else {
+        cerr << "Не удалось открыть файл для записи: " << fileName << endl;}
 }
 
 int main()
@@ -26,7 +33,7 @@ int main()
     string outputFileName = "output.txt";
 
     vector<string> lines = readFile(inputFileName);
-    Output(lines);
+    output(lines);
     writeToFile(lines, outputFileName);
 
     cout << "Программа выполнена";
